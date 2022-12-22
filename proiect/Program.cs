111ab930f39399deae1250d6a07586d2307bd4ab
Dorina <acsinoi.dorina@yandex.ru>
2022-12-22 13:49:54 +0300

    algoritm

--- proiect/Program.cs
+++ proiect/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[8] {"4569", "2", "hello", "45", "world", "ups","res","final"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(array1, array2);
+ShowArray(array2);
